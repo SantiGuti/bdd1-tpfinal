edd4eb6ed055fafd04dfcdeb6c36fd6caee5d044
Daniela Rages <danu.rages@hotmail.com.ar>
2022-06-10 21:00:36 -0300

    Agrego cosas al menu (ahora en la funcion main) limpié código y agregue funciones.

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-
 	_ "github.com/lib/pq"
 )
 
@@ -16,31 +15,52 @@ type cliente struct {
 }
 
 func main() {
-	createDatabase() //Crea una nueva base de datos
-	menu()
-	//Conecta con la base de datos creada
+	//Despliega el menú visible al usuario
+	var nombre string
+	fmt.Printf("Escriba su nombre: ")
+	fmt.Scanf("%s", &nombre)
+	fmt.Printf("Hola, %s\n",nombre)
+	fmt.Printf("Seleccione un número del siguiente menú:\n")
+	fmt.Printf("1. Crear una nueva base de datos.\n")
+	fmt.Printf("2. Crear las tablas.\n")
+	fmt.Printf("3. Completar las tablas.\n")
+	fmt.Printf("4. Asignar las PK y FK.\n")
+	fmt.Printf("5. Borrar las PK y FK.\n")
+	fmt.Printf("6. Crear las stored procedures y los triggers.\n")
+	fmt.Printf("7. Autorizar las compras.\n")
+	fmt.Printf("8. Generar el resumen de la compra.\n")
+	fmt.Printf("9. Generar BoldDB.\n")
+	var selec int
+	fmt.Scanf("%s", &selec)
+	if selec == 1 {
+		fmt.Printf("\nUsted ha seleccionado la opción 1: Crear una base de datos.\n")
+	}
+	if selec == 2 {
+		fmt.Printf("\nUsted ha seleccionado la opción 2: Crear las tablas.\n")
+	}
+	if selec == 3 {
+		fmt.Printf("\nUsted ha seleccionado la opción 3: Completar las tablas.\n")	
+	}
+	//Abre la conexión a la base de datos
 	db, err := sql.Open("postgres", "user=postgres host=localhost dbname=tarjetascredito sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
-
 	}
 	defer db.Close()
-
+	//Crea una nueva base de datos
+	createDatabase() 
 	//Lee las tablas (Modelos de datos)
 	_, err = db.Exec(leerArchivo("tablas.sql"))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	//Carga las tablas con los datos
 	_, err = db.Exec(leerArchivo("datos.sql"))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	////////////////////////////////////////////////////////////////
 	// print de prueba lectura tabla cliente //
-
 	rows, err := db.Query(`select * from cliente`)
 	if err != nil {
 		log.Fatal(err)
@@ -59,37 +79,6 @@ func main() {
 
 }
 
-/*_, err = db.Exec(`create table cliente(nrocliente int, nombre text, apellido text, domicilio text, telefono char(12))`)
-if err != nil {
-	log.Fatal(err)
-}*/
-
-/*	_, err = db.Exec(`insert into cliente values (1, 'Juan', 'Lopez', 'Urquiza 2629', '1157845695');
-	                  insert int cliente values (2, 'Maria', 'Ramirez', 'Oribe 1576', '1159674130')`) //ejemplo
-
-	if err != nil {
-		log.Fatal(err)
-	}*/
-
-/*
-	rows, err := db.Query(`select * from cliente`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	var c cliente
-
-	for rows.Next() {
-		if err := rows.Scan(&c.nrocliente, &c.nombre, &c.apellido, &c.domicilio, &c.telefono); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%v %v %v\n", c.nrocliente, c.nombre, c.apellido, c.domicilio, c.telefono)
-	}
-	if err = rows.Err(); err != nil {
-		log.Fatal(err)
-	}*/
-
 func createDatabase() {
 	db, err := sql.Open("postgres", "user=postgres host=localhost dbname=postgres sslmode=disable")
 	if err != nil {
@@ -105,6 +94,7 @@ func createDatabase() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("\nNueva base de datos creada.\n")
 }
 
 func leerArchivo(archivo string) string {
@@ -116,21 +106,13 @@ func leerArchivo(archivo string) string {
 	return ret
 }
 
-func menu(){
-
-	var nombre string
-	fmt.Printf("Escriba su nombre: ")
-	fmt.Scanf("%s", &nombre)
-	fmt.Printf("Hola, %s\n",nombre)
-	fmt.Printf("Seleccione un número del siguiente menú:\n")
-	fmt.Printf("1. Crear una nueva base de datos.\n")
-	fmt.Printf("2. Crear las tablas.\n")
-	fmt.Printf("3. Completar las tablas.\n")
-	fmt.Printf("4. Asignar las PK y FK.\n")
-	fmt.Printf("5. Borrar las PK y FK.\n")
-	fmt.Printf("6. Crear las stored procedures y los triggers.\n")
-	fmt.Printf("7. Autorizar las compras.\n")
-	fmt.Printf("8. Generar el resumen de la compra.\n")
-	fmt.Printf("9. Generar BoldDB.\n")
-
+func mostrarDatos(archivo string) string {
+	tablas, err := ioutil.ReadFile(archivo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	contenido := string(tablas)
+	fmt.Printf("%s", contenido)
+	ret := string(tablas)
+	return ret
 }
