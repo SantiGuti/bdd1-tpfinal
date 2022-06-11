0d46174e993b6a010ab4ce71bc42406e3d0ebb44
Daniela Rages <danu.rages@hotmail.com.ar>
2022-06-10 23:46:51 -0300

    Un poco mas de las opciones del menu en main

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"log"
 	_ "github.com/lib/pq"
 )
-
+//
 type cliente struct {
 	nrocliente                  int
 	nombre, apellido, domicilio string
@@ -15,6 +15,12 @@ type cliente struct {
 }
 
 func main() {
+	//Abre la conexión a la base de datos
+	db, err := sql.Open("postgres", "user=postgres host=localhost dbname=tarjetascredito sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 	//Despliega el menú visible al usuario
 	var nombre string
 	fmt.Printf("Escriba su nombre: ")
@@ -32,40 +38,67 @@ func main() {
 	fmt.Printf("9. Generar BoldDB.\n")
 	var selec int
 	fmt.Scanf("%s", &selec)
+	//OPCIÓN 1
 	if selec == 1 {
 		fmt.Printf("\nUsted ha seleccionado la opción 1: Crear una base de datos.\n")
 	}
+	//Crea una nueva base de datos
+	createDatabase()
+	//OPCIÓN 2
 	if selec == 2 {
 		fmt.Printf("\nUsted ha seleccionado la opción 2: Crear las tablas.\n")
 	}
-	if selec == 3 {
-		fmt.Printf("\nUsted ha seleccionado la opción 3: Completar las tablas.\n")	
-	}
-	//Abre la conexión a la base de datos
-	db, err := sql.Open("postgres", "user=postgres host=localhost dbname=tarjetascredito sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	//Crea una nueva base de datos
-	createDatabase() 
 	//Lee las tablas (Modelos de datos)
 	_, err = db.Exec(leerArchivo("tablas.sql"))
 	if err != nil {
 		log.Fatal(err)
+	}
+	//OPCIÓN 3
+	if selec == 3 {
+		fmt.Printf("\nUsted ha seleccionado la opción 3: Completar las tablas.\n")	
+	}
+	//OPCIÓN 4
+	if selec == 4{
+		fmt.Printf("\nUsted ha seleccionado la opción 4: Asignar las PK y FK.\n")
+	}
+	
+	//OPCIÓN 5
+	if selec == 5{
+		fmt.Printf("\nUsted ha seleccionado la opción 5: Borrar las PK y FK.\n")
+	}
+
+	//OPCIÓN 6
+	if selec == 6{
+		fmt.Printf("\nUsted ha seleccionado la opción 6: Crear las stored procedures y los triggers.\n")
+	}
+
+	//OPCIÓN 7
+	if selec == 7{
+		fmt.Printf("\nUsted ha seleccionado la opción 7: Autorizar las compras.\n")
+	}
+
+	//OPCIÓN 8
+	if selec == 8{
+		fmt.Printf("\nUsted ha seleccionado la opción 8: Generar el resumen de la compra.\n")
+	}
+
+	//OPCIÓN 9
+	if selec == 9{
+		fmt.Printf("\nUsted ha seleccionado la opción 9: Generar BoldDB.\n")
 	}
 	//Carga las tablas con los datos
 	_, err = db.Exec(leerArchivo("datos.sql"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	////////////////////////////////////////////////////////////////
-	// print de prueba lectura tabla cliente //
+	//ESCANEA E IMPRIME LOS DATOS DE LA TABLA CLIENTE	
+	//Lectura de la tabla cliente
 	rows, err := db.Query(`select * from cliente`)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
+	//Scan de los datos contenidos en la tabla
 	var c cliente
 	for rows.Next() {
 		if err := rows.Scan(&c.nrocliente, &c.nombre, &c.apellido, &c.domicilio, &c.telefono); err != nil {
@@ -76,7 +109,6 @@ func main() {
 	if err = rows.Err(); err != nil {
 		log.Fatal(err)
 	}
-
 }
 
 func createDatabase() {
