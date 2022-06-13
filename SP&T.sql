c0bbd0bcc84ff67149e65313f36257a3e1ef3027
SantiGuti <santiagogutierrez1905@gmail.com>
2022-06-13 03:38:18 -0300

    un caso mas en sp hecho

--- SP&T.sql
+++ SP&T.sql
@@ -12,11 +12,13 @@ begin
     select * into resultado from tarjeta where codseguridad = vcodseguridad;
     if not found then
         insert into rechazo values(8456, vnrotarjeta, vnrocomercio, '2021-08-24 13:32:58', vmonto, "codigo de seguridad invalido");
+        return false;
     end if;
 
-    select * into resultado from compra where pagado = false;
+    select * into resultado from compra where pagado = false and nrotarjeta = vnrotarjeta SUM(monto) + vmonto > (select limitecompra from tarjeta where nrotarjeta = vnrotarjeta);
     if not found then
-        insert into rechazo values(8456, vnrotarjeta, vnrocomercio, '2021-08-24 13:32:58', vmonto, "codigo de seguridad invalido");
+        insert into rechazo values(8456, vnrotarjeta, vnrocomercio, '2021-08-24 13:32:58', vmonto, "supera limite de compra");
+        return false;
     end if;
 
     insert into compra values (84561, vnrotarjeta, vnrocomercio, '2021-08-24 13:32:58', vmonto, true);
