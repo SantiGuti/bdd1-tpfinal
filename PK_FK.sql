065f6463274372b0970d029d204d07a43bcead8e
SantiGuti <santiagogutierrez1905@gmail.com>
2022-06-16 14:46:42 -0300

    coreccion nombre variable año->anio

--- PK_FK.sql
+++ PK_FK.sql
@@ -4,7 +4,7 @@ alter table tarjeta	add constraint tarjeta_pk 	primary key (nrotarjeta);
 alter table comercio add constraint comercio_pk primary key (nrocomercio);
 alter table compra	add constraint compra_pk	primary key (nrooperacion);
 alter table rechazo	add constraint rechazo_pk	primary key (nrorechazo);
-alter table cierre	add constraint cierre_pk	primary key (año, mes, terminacion);
+alter table cierre	add constraint cierre_pk	primary key (anio, mes, terminacion);
 alter table cabecera add constraint cabecera_pk	primary key (nroresumen);
 alter table detalle	add constraint detalle_pk	primary key (nroresumen, nrolinea);
 alter table alerta	add constraint alerta_pk	primary key (nroalerta);
